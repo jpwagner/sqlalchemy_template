48717f8177dbdcd0d4e9c77a904a99447eea9727
Jim Wagner <jpwagner@mit.edu>
2013-11-15 15:04:59 -0500

    create db script

---
+++ db/create_tables.py
@@ -0,0 +1,25 @@
+from db import models
+
+def create_tables(tables):
+	for table in tables:
+		table.create_table()
+	return None
+
+def drop_tables(tables):
+	for table in tables:
+		table.drop_table()
+	return None
+
+def create_all_tables():
+	tables = []
+
+	tables.append(models.SomeTable)
+
+	try:
+		drop_tables(tables)
+	except:
+		pass
+	create_tables(tables)
+
+
+create_all_tables()
